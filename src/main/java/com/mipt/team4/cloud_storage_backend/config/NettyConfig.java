b016095cf8b778a2aa0a48c8b9fc26e327b550e7
Sergey Rezchikov <rez4.off@example.com>
2025-11-03 00:21:16 +0300

    feat: add Netty config class and netty server

--- src/main/java/com/mipt/team4/cloud_storage_backend/config/NettyConfig.java
+++ src/main/java/com/mipt/team4/cloud_storage_backend/config/NettyConfig.java
@@ -1,5 +1,7 @@
 package com.mipt.team4.cloud_storage_backend.config;
 
+import com.mipt.team4.cloud_storage_backend.config.sources.ConfigSource;
+
 public class NettyConfig {
   private final int port;
   private final int bossThreads;

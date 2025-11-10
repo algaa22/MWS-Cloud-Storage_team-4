ee4ee6a5afd9d593321a2961e028778b559d93c4
Starosvitskii Pavel <starosvitskii.po@phystech.edu>
2025-11-10 23:47:28 +0300

    feat: add createBucket()

--- src/main/java/com/mipt/team4/cloud_storage_backend/config/StorageConfig.java
+++ src/main/java/com/mipt/team4/cloud_storage_backend/config/StorageConfig.java
@@ -5,17 +5,19 @@ import com.mipt.team4.cloud_storage_backend.config.sources.ConfigSource;
 public class StorageConfig {
   private final long maxFileSize;
   private final long maxFileChunkSize;
+  private final String userDataBucketName;
 
-  public StorageConfig(long maxFileSize, long maxFileChunkSize) {
+  public StorageConfig(long maxFileSize, long maxFileChunkSize, String userDataBucketName) {
     this.maxFileSize = maxFileSize;
     this.maxFileChunkSize = maxFileChunkSize;
+    this.userDataBucketName = userDataBucketName;
   }
 
   public static StorageConfig from(ConfigSource source) {
     return new StorageConfig(
-            source.getLong("storage.controller.max-file-size").orElseThrow(),
-            source.getLong("storage.controller.max-file-chunk-size").orElseThrow()
-    );
+        source.getLong("storage.controller.max-file-size").orElseThrow(),
+        source.getLong("storage.controller.max-file-chunk-size").orElseThrow(),
+        source.getString("storage.repository.user-data-bucket.name").orElseThrow());
   }
 
   public long getMaxFileSize() {
@@ -24,5 +26,9 @@ public class StorageConfig {
 
   public long getMaxFileChunkSize() {
     return maxFileChunkSize;
+  }
+
+  public String getUserDataBucketName() {
+    return userDataBucketName;
   }
 }
--- src/main/java/com/mipt/team4/cloud_storage_backend/repository/storage/MinioContentRepository.java
+++ src/main/java/com/mipt/team4/cloud_storage_backend/repository/storage/MinioContentRepository.java
@@ -2,29 +2,51 @@ package com.mipt.team4.cloud_storage_backend.repository.storage;
 
 import com.mipt.team4.cloud_storage_backend.config.DatabaseConfig;
 import com.mipt.team4.cloud_storage_backend.config.MinioConfig;
+import com.mipt.team4.cloud_storage_backend.config.StorageConfig;
+import io.minio.BucketExistsArgs;
 import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.errors.*;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 public class MinioContentRepository implements FileContentRepository {
   private final MinioConfig config;
+  private MinioClient minioClient;
 
   public MinioContentRepository(MinioConfig config) {
     this.config = config;
   }
 
+  public void createBucket() {
+      try {
+          boolean bucketFound =
+              minioClient.bucketExists(
+                  BucketExistsArgs.builder()
+                      .bucket("")
+                      .build());
+      } catch (ErrorResponseException | InsufficientDataException | InternalException | InvalidKeyException |
+               InvalidResponseException | IOException | NoSuchAlgorithmException | ServerException |
+               XmlParserException e) {
+          throw new RuntimeException(e);
+      }
+  }
+
   @Override
   public void initialize() {
-      try {
-      MinioClient minioClient =
+    try {
+      minioClient =
           MinioClient.builder()
               .endpoint(config.getUrl())
               .credentials(config.getUsername(), config.getPassword())
               .build();
-      } catch (Exception e) {
-          throw new RuntimeException(e);
-      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override

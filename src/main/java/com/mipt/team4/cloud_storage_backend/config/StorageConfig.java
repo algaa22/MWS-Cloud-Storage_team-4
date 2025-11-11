2436cb1f0df6ae1eab2d1c405c3b5d5bb7686851
Sergey Rezchikov <rez4.off@example.com>
2025-11-11 12:46:30 +0300

    chore: config singletons are enum now

--- src/main/java/com/mipt/team4/cloud_storage_backend/config/StorageConfig.java
+++ src/main/java/com/mipt/team4/cloud_storage_backend/config/StorageConfig.java
@@ -3,56 +3,41 @@ package com.mipt.team4.cloud_storage_backend.config;
 import com.mipt.team4.cloud_storage_backend.config.sources.ConfigSource;
 import com.mipt.team4.cloud_storage_backend.config.sources.EnvironmentConfigSource;
 
-public class StorageConfig {
-  private static volatile StorageConfig instance;
+public enum StorageConfig {
+  INSTANCE;
+
   private final long maxFileSize;
   private final int maxFileChunkSize;
   private final int maxContentLength;
   private final int fileDownloadChunkSize;
   private final int sendUploadProgressInterval;
 
-  public StorageConfig(long maxFileSize, int maxFileChunkSize, int maxContentLength, int fileDownloadChunkSize, int sendUploadProgressInterval) {
-    this.maxFileSize = maxFileSize;
-    this.maxFileChunkSize = maxFileChunkSize;
-    this.maxContentLength = maxContentLength;
-    this.fileDownloadChunkSize = fileDownloadChunkSize;
-    this.sendUploadProgressInterval = sendUploadProgressInterval;
-  }
+  StorageConfig() {
+    ConfigSource source = new EnvironmentConfigSource();
 
-  public static StorageConfig getInstance() {
-    if (instance == null) {
-      synchronized (DatabaseConfig.class) {
-        if (instance == null) {
-          ConfigSource source = new EnvironmentConfigSource();
-
-          instance = new StorageConfig(
-                  source.getLong("storage.http.max-file-size").orElseThrow(),
-                  source.getInt("storage.http.max-file-chunk-size").orElseThrow(),
-                  source.getInt("storage.http.file-download-chunk-size").orElseThrow(),
-                  source.getInt("storage.http.max-content-length").orElseThrow(),
-                  source.getInt("storage.http.send-upload-progress-interval").orElseThrow()
-          );
-        }
-      }
-    }
-
-    return instance;
+    this.maxFileSize = source.getLong("storage.http.max-file-size").orElseThrow();
+    this.maxFileChunkSize = source.getInt("storage.http.max-file-chunk-size").orElseThrow();
+    this.maxContentLength = source.getInt("storage.http.max-content-length").orElseThrow();
+    this.fileDownloadChunkSize =
+        source.getInt("storage.http.file-download-chunk-size").orElseThrow();
+    this.sendUploadProgressInterval =
+        source.getInt("storage.http.send-upload-progress-interval").orElseThrow();
   }
 
   public long getMaxFileSize() {
     return maxFileSize;
   }
 
-  public long getMaxFileChunkSize() {
+  public int getMaxFileChunkSize() {
     return maxFileChunkSize;
-  }
-
-  public int getFileDownloadChunkSize() {
-    return fileDownloadChunkSize;
   }
 
   public int getMaxContentLength() {
     return maxContentLength;
+  }
+
+  public int getFileDownloadChunkSize() {
+    return fileDownloadChunkSize;
   }
 
   public int getSendUploadProgressInterval() {

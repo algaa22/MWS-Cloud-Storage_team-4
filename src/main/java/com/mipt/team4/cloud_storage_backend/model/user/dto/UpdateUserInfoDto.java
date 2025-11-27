3eeb53bbc3b731c96b358ddcd770983b6d7a2193
Sergey Rezchikov <rez4.off@example.com>
2025-11-27 23:28:06 +0300

    feat: create update user info dto

--- src/main/java/com/mipt/team4/cloud_storage_backend/model/user/dto/UpdateUserInfoDto.java
+++ src/main/java/com/mipt/team4/cloud_storage_backend/model/user/dto/UpdateUserInfoDto.java
@@ -1,3 +1,5 @@
 package com.mipt.team4.cloud_storage_backend.model.user.dto;
 
-public record UpdateUserInfoDto (String accessToken, String oldPassword, String newPassword, String newName) {}
+import java.util.Optional;
+
+public record UpdateUserInfoDto (String accessToken, Optional<String> oldPassword, Optional<String> newPassword, Optional<String> newName) {}

5ef1b5dac36d2be60bec1afb0c30c5a9ad4dd003
cmetanna <rez4.off@gmail.com>
2026-03-04 00:20:36 +0300

    chore: code-style format

--- src/main/java/com/mipt/team4/cloud_storage_backend/exception/retry/CompleteUploadRetriableException.java
+++ src/main/java/com/mipt/team4/cloud_storage_backend/exception/retry/CompleteUploadRetriableException.java
@@ -1,6 +1,5 @@
 package com.mipt.team4.cloud_storage_backend.exception.retry;
 
-
 public class CompleteUploadRetriableException extends UploadRetriableException {
   public CompleteUploadRetriableException(Throwable cause) {
     super("COMPLETE UPLOAD", cause);

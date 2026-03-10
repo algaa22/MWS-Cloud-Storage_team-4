e44edad0032cef86aed9a7eec13e851dc39e2aed
algaaa <anikanova.aa@phystech.edu>
2026-03-10 17:47:27 +0300

    fix: fix code

---
+++ src/main/java/com/mipt/team4/cloud_storage_backend/model/storage/dto/requests/StartChunkedUploadRequest.java
@@ -0,0 +1,29 @@
+package com.mipt.team4.cloud_storage_backend.model.storage.dto.requests;
+
+import com.mipt.team4.cloud_storage_backend.exception.validation.ValidationFailedException;
+import com.mipt.team4.cloud_storage_backend.service.user.security.JwtService;
+import com.mipt.team4.cloud_storage_backend.utils.validation.ValidationResult;
+import com.mipt.team4.cloud_storage_backend.utils.validation.Validators;
+
+import java.util.List;
+import java.util.Optional;
+
+public record StartChunkedUploadRequest(
+    String sessionId,
+    String userToken,
+    Optional<String> parentId,
+    String name,
+    List<String> tags,
+    int size) {
+
+  public void validate(JwtService jwtService) throws ValidationFailedException {
+    ValidationResult result =
+        Validators.all(
+            Validators.mustBePositive("File size", size),
+            Validators.notBlank("Session ID", sessionId),
+            Validators.validToken(jwtService, userToken),
+            Validators.validFileName("File Name", name));
+
+    Validators.throwExceptionIfNotValid(result);
+  }
+}

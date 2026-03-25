0a141e12645829110e13fda6628585722ae7f9c4
Starosvitskii Pavel <starosvitskii.po@phystech.edu>
2026-03-25 14:55:49 +0300

    chore: remove unnecessary status() method

--- src/main/java/com/mipt/team4/cloud_storage_backend/model/storage/dto/responses/HealthCheckResponse.java
+++ src/main/java/com/mipt/team4/cloud_storage_backend/model/storage/dto/responses/HealthCheckResponse.java
@@ -1,6 +1,5 @@
 package com.mipt.team4.cloud_storage_backend.model.storage.dto.responses;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mipt.team4.cloud_storage_backend.model.storage.enums.ComponentStatus;
 import com.mipt.team4.cloud_storage_backend.model.storage.enums.MemoryStatus;
 import com.mipt.team4.cloud_storage_backend.model.storage.enums.OverallStatus;
@@ -9,14 +8,11 @@ import com.mipt.team4.cloud_storage_backend.netty.mapping.annotations.response.R
 import io.netty.handler.codec.http.HttpResponseStatus;
 
 public record HealthCheckResponse(
-    @JsonIgnore @ResponseStatus HttpResponseStatus httpStatus,
+    @ResponseStatus HttpResponseStatus httpStatus,
     @ResponseBodyParam OverallStatus status,
     @ResponseBodyParam ComponentStatus database,
     @ResponseBodyParam ComponentStatus s3,
     @ResponseBodyParam MemoryStatus memory,
     @ResponseBodyParam String timestamp) {
-  @Override
-  public OverallStatus status() {
-    return status;
-  }
+
 }

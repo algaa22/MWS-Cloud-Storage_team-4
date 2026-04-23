de3bc6665d301be39caae176f265a214cebdd3dc
cmetanna <rez4.off@gmail.com>
2026-04-23 21:59:42 +0300

    chore: add antivirus metrics

--- src/main/java/com/mipt/team4/cloud_storage_backend/config/constants/storage/StorageMetrics.java
+++ src/main/java/com/mipt/team4/cloud_storage_backend/config/constants/storage/StorageMetrics.java
@@ -7,4 +7,5 @@ public class StorageMetrics {
   public static final String FILES_STATUS_COUNT = "storage.files.status.count";
   public static final String UPLOAD_CHUNKS_ACTIVE = "storage.upload.chunks.active";
   public static final String CLEANUP_STALE_FILES = "storage.cleanup.stale.files";
+  public static final String ANTIVIRUS_SCAN_COUNT = "antivirus.scan.count";
 }
